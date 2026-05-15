901a055c7038236714ad974847a244ba04a36875
Victor Pudeyev <piousbox@gmail.com>
2026-05-15 08:34:14 -0500

    can exclude live streams

--- src/Controller/YoutubeChannelsController.php
+++ src/Controller/YoutubeChannelsController.php
@@ -31,9 +31,14 @@ class YoutubeChannelsController extends ControllerBase {
     $tags_issue_ids = array_column($tags_issue_ids, 'target_id');
     logg($tags_issue_ids, '$tags_issue_ids');
 
-    $channel_id     = $youtube_channel->get('field_channel_id')->value;
+    $channel_id      = $youtube_channel->get('field_channel_id')->value;
+    $include_streams = (bool) $youtube_channel->get('field_include_streams')->value;
     $n_videos = 10;
-    $url = 'https://www.googleapis.com/youtube/v3/search?key='.$api_key.'&channelId='.$channel_id.'&part=snippet,id&order=date&maxResults='.$n_videos.'&videoDuration=long&type=video';
+    $url = 'https://www.googleapis.com/youtube/v3/search?key='
+      . $api_key
+      . '&channelId='.$channel_id
+      . '&part=snippet,id&order=date&maxResults='.$n_videos
+      . '&videoDuration=long&type=video';
     logg($url, '$url');
 
     $json = file_get_contents($url);
@@ -47,6 +52,22 @@ class YoutubeChannelsController extends ControllerBase {
 
       $youtube_id = $item->id->videoId;
       $youtube_title = YoutubeVideo::title($youtube_id);
+
+      /*
+       * Check if this was a livestream
+      **/
+      if (!$include_streams) {
+        $video_url = 'https://www.googleapis.com/youtube/v3/videos'
+          . '?key=' . $api_key
+          . '&id=' . $youtube_id
+          . '&part=liveStreamingDetails';
+        $video_json = file_get_contents($video_url);
+        $video_data = json_decode($video_json);
+        if (!empty($video_data->items[0]->liveStreamingDetails)) {
+          continue;
+        }
+      }
+
 
       // $issue_uuid = '35'; // '4ac9695b-0854-4972-8528-1f52e21d2235'; // taxonomy_term/35 :: 2024q1-issue
       $node_manager  = \Drupal::entityTypeManager()->getStorage('node');
